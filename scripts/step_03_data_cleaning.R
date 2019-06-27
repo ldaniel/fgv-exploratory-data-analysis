18cc4d8a7dd60e7df5c22d794b48b486e9f63eed
ldaniel <contato@leandrodaniel.com>
2019-06-26 21:59:43 -0300

    Looking for NA and empty cells.

--- scripts/step_03_data_cleaning.R
+++ scripts/step_03_data_cleaning.R
@@ -1,13 +1,34 @@
 # analysing missing values and other strange conditions -----------------------
-temp <- filter(transaction, is.na(account))
-temp <- filter(transaction, operation == "", bank == "", !is.na(type), is.na(account))
-temp <- filter(transaction, operation == "")
-temp <- filter(transaction, bank == "")
-temp <- filter(transaction, k_symbol == "")
-temp <- filter(transaction, is.na(account))
 
-View(temp)
-summary(transaction)
-str(transaction)
+# looking for NA's in any column
+sapply(client, function(x) sum(is.na(x)))
+sapply(disposition, function(x) sum(is.na(x)))
+sapply(district, function(x) sum(is.na(x)))
+sapply(creditcard, function(x) sum(is.na(x)))
+sapply(account, function(x) sum(is.na(x)))
+sapply(loan, function(x) sum(is.na(x)))
+sapply(permanent_order, function(x) sum(is.na(x)))
 
-matrixplot(transaction)
+# only the transaction table has 760931 NA's in account column
+sapply(transaction, function(x) sum(is.na(x))) 
+
+# looking for empty cells in any column
+sapply(client, function(x) table(as.character(x) =="")["TRUE"])
+sapply(disposition, function(x) table(as.character(x) =="")["TRUE"])
+sapply(district, function(x) table(as.character(x) =="")["TRUE"])
+sapply(creditcard, function(x) table(as.character(x) =="")["TRUE"])
+sapply(account, function(x) table(as.character(x) =="")["TRUE"])
+sapply(loan, function(x) table(as.character(x) =="")["TRUE"])
+sapply(permanent_order, function(x) table(as.character(x) =="")["TRUE"]) 
+
+# only the transaction table has empty values, in the following columns:
+#   operation = 183114 empty cells
+#   k_symbol  = 481881 empty cells
+#   bank      = 782812 empty cells
+sapply(transaction, function(x) table(as.character(x) =="")["TRUE"])
+
+# View(temp)
+# summary(transaction)
+# str(transaction)
+
+# matrixplot(transaction)
