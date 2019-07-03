35dd5a8af28b98c66b71382a95289e19f74aecd0
RodriGonca <rodrigo.goncalves@me.com>
2019-07-03 17:35:10 -0300

    Adding account_transaction_pattern table to be used on the logist regression create to predict loan default.

--- scripts/step_05_data_enhancement.R
+++ scripts/step_05_data_enhancement.R
@@ -22,3 +22,58 @@ account_balance <- arrange(transaction, desc(date), account_id) %>%
   dplyr::select(account_id, date, balance, avg_balance)
 
 colnames(account_balance) <- c("account_id", "last_transaction_date", 'account_balance', 'avg_balance')
+
+# calculate some derived variables from transaction table to feed our logist regression model.
+
+transaction.date.ini = make_date(1993, 01, 01)
+transaction.date.end = make_date(1998, 12, 31)
+
+account_transaction_pattern <- group_by(transaction, account_id) %>% 
+  summarise(transaction_count = n(),
+            transaction_amount = sum(amount),
+            last_transaction_date = max(date),
+            transaction_amount_date_filter = crossprod(amount,  
+                                                       date >= transaction.date.ini &
+                                                         date <= transaction.date.end),
+            
+            percent_amount_old_age_pension = crossprod(amount, 
+                                                       date >= transaction.date.ini &
+                                                         date <= transaction.date.end &
+                                                         k_symbol == 'old age pension') / 
+              transaction_amount_date_filter,
+            percent_amount_insurance_payment = crossprod(amount, 
+                                                         date >= transaction.date.ini &
+                                                           date <= transaction.date.end &
+                                                           k_symbol == 'insurrance payment') / 
+              transaction_amount_date_filter,
+            percent_amount_sanction_interest = crossprod(amount, 
+                                                         date >= transaction.date.ini &
+                                                           date <= transaction.date.end &
+                                                           k_symbol == 'sanction interest') /
+              transaction_amount_date_filter,
+            percent_amount_household = crossprod(amount, 
+                                                 date >= transaction.date.ini &
+                                                   date <= transaction.date.end &
+                                                   k_symbol == 'household') / 
+              transaction_amount_date_filter,
+            percent_amount_statement = crossprod(amount, 
+                                                 date >= transaction.date.ini &
+                                                   date <= transaction.date.end &
+                                                   k_symbol == 'statement') / 
+              transaction_amount_date_filter,
+            percent_amount_interest_credited = crossprod(amount, 
+                                                         date >= transaction.date.ini &
+                                                           date <= transaction.date.end &
+                                                           k_symbol == 'interest credited') / 
+              transaction_amount_date_filter,
+            percent_amount_loan_payment = crossprod(amount, 
+                                                    date >= transaction.date.ini &
+                                                      date <= transaction.date.end &
+                                                      k_symbol == 'loan payment') / 
+              transaction_amount_date_filter,
+            percent_amount_other = crossprod(amount, 
+                                             date >= transaction.date.ini &
+                                               date <= transaction.date.end &
+                                               k_symbol == '') /
+              transaction_amount_date_filter
+  )
