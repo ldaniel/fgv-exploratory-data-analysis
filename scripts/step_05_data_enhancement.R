0b9eba770041d0d0dd7b22914154c21bfe06b09c
ldaniel <contato@leandrodaniel.com>
2019-07-03 02:18:47 -0300

    First attempt to work with maps.

--- scripts/step_05_data_enhancement.R
+++ scripts/step_05_data_enhancement.R
@@ -11,5 +11,5 @@ loan <- mutate(loan, defaulter = as.logical( plyr::mapvalues(status, c ('A','B',
                contract_status = plyr::mapvalues(status, c ('A','B','C','D'), c('finished','finished','running','running')),
                type = 'Owner')
 
-# improving client data by havivng its age group
+# improving client data by having its age group
 client <- mutate(client, age_bin = paste(findInterval(age, c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)) * 10,'+'))
