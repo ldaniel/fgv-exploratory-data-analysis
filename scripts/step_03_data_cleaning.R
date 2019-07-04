12ff093540abf3570d0ea6df35ad44a2c6ba00a4
ldaniel <contato@leandrodaniel.com>
2019-07-03 22:50:58 -0300

    Data cleaning script only gets the important analysis right now.

--- scripts/step_03_data_cleaning.R
+++ scripts/step_03_data_cleaning.R
@@ -4,30 +4,29 @@
 transaction$k_symbol = trimws(transaction$k_symbol)
 
 # looking for NA's in any column
-sapply(client, function(x) sum(is.na(x)))
-sapply(disposition, function(x) sum(is.na(x)))
-sapply(district, function(x) sum(is.na(x)))
-sapply(creditcard, function(x) sum(is.na(x)))
-sapply(account, function(x) sum(is.na(x)))
-sapply(loan, function(x) sum(is.na(x)))
-sapply(permanent_order, function(x) sum(is.na(x)))
+# sapply(client, function(x) sum(is.na(x)))
+# sapply(disposition, function(x) sum(is.na(x)))
+# sapply(district, function(x) sum(is.na(x)))
+# sapply(creditcard, function(x) sum(is.na(x)))
+# sapply(account, function(x) sum(is.na(x)))
+# sapply(loan, function(x) sum(is.na(x)))
+# sapply(permanent_order, function(x) sum(is.na(x)))
 
 # only the transaction table has 760931 NA's in account column
-sapply(transaction, function(x) sum(is.na(x))) 
+transaction_na_cols <- sapply(transaction, function(x) sum(is.na(x)))
 
 # looking for empty cells in any column
-sapply(client, function(x) table(as.character(x) =="")["TRUE"])
-sapply(disposition, function(x) table(as.character(x) =="")["TRUE"])
-sapply(district, function(x) table(as.character(x) =="")["TRUE"])
-sapply(creditcard, function(x) table(as.character(x) =="")["TRUE"])
-sapply(account, function(x) table(as.character(x) =="")["TRUE"])
-sapply(loan, function(x) table(as.character(x) =="")["TRUE"])
-sapply(permanent_order, function(x) table(as.character(x) =="")["TRUE"]) 
+# sapply(client, function(x) table(as.character(x) =="")["TRUE"])
+# sapply(disposition, function(x) table(as.character(x) =="")["TRUE"])
+# sapply(district, function(x) table(as.character(x) =="")["TRUE"])
+# sapply(creditcard, function(x) table(as.character(x) =="")["TRUE"])
+# sapply(account, function(x) table(as.character(x) =="")["TRUE"])
+# sapply(loan, function(x) table(as.character(x) =="")["TRUE"])
+# sapply(permanent_order, function(x) table(as.character(x) =="")["TRUE"]) 
 
 # only the transaction table has empty values, in the following columns:
 #   operation = 183114 empty cells
-#   k_symbol  = 481881 empty cells
+#   k_symbol  = 535314 empty cells
 #   bank      = 782812 empty cells
-sapply(transaction, function(x) table(as.character(x) =="" | as.character(x) ==" ")["TRUE"])
-
+transaction_empty_cols <-  sapply(transaction, function(x) table(as.character(x) =="" | as.character(x) ==" ")["TRUE"])
 
