e331cb480ae6050197828c69d58a2f05b3fc16a5
ldaniel <contato@leandrodaniel.com>
2019-07-05 17:21:16 -0300

    No more knit button on this project. =)

--- scripts/generate_markdown_website.R
+++ scripts/generate_markdown_website.R
@@ -3,14 +3,24 @@ scriptsDirectoryPath  <- dirname(rstudioapi::getSourceEditorContext()$path)
 markdownDirectoryPath <- stringr::str_replace(scriptsDirectoryPath, "/scripts", "/markdown")
 docsDirectoryPath     <- stringr::str_replace(scriptsDirectoryPath, "/scripts", "/docs")
 tempSiteDirecoryPath  <- paste(markdownDirectoryPath, "/_site", sep = "")
+rootDirectoryPath     <- str_replace(scriptsDirectoryPath, "/scripts", "")
 
 # generating website from markdown files --------------------------------------
 setwd(markdownDirectoryPath)
 rmarkdown::render_site()
 
 # moving website files to docs directory --------------------------------------
-filesstrings::file.move(paste(tempSiteDirecoryPath, "/*.*", sep = ""), docsDirectoryPath)
-filesstrings::dir.remove(tempSiteDirecoryPath)
+if(file.exists(docsDirectoryPath))
+{
+  unlink(docsDirectoryPath, recursive=TRUE)
+  file.remove(docsDirectoryPath)
+}
+
+if(file.exists(tempSiteDirecoryPath))
+{
+  file.rename(from = tempSiteDirecoryPath,  to = docsDirectoryPath)
+}
 
 # setting the working directory to the original one ---------------------------
-source("./scripts/step_00_config_environment.R")
+setwd(rootDirectoryPath)
+getwd()
